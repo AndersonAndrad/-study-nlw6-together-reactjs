94c6a22bbd2e4ebfb7210d0fe92e43dbde23049a
AndersonAndrad <anderson_andrade_@outlook.com>
2021-06-22 08:01:09 -0300

    create the project base, with create react-app typescript template, missing create the routes pages

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>bora coda</h1>
+    </div>
+  );
+}
+
+export default App;
+
+//TODO: Stop in 1:08 minutes of video
