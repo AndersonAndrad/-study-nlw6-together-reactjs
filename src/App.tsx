4ee07cabce60a12bf0dcd9c5e6de1e455a1b0f25
AndersonAndrad <anderson_andrade_@outlook.com>
2021-06-23 19:56:17 -0300

    config - adding the firebase configuration - using  enviromnents variables

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,9 @@ import React from 'react';
 function App() {
   return (
     <div className="App">
-      <h1>bora coda</h1>
+      <h1>go to programming</h1>
     </div>
   );
 }
 
 export default App;
-
-//TODO: Stop in 1:08 minutes of video
